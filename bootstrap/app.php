1679fc7ebed64c92ea1180696b987afb8fec2207
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2024-11-24 21:11:33 +0100

    Developing forms for adding new data to tables with server-side validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Create The 
+| Create T 
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
