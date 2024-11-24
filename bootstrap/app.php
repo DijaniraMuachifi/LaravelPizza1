1abc6fc583f2f648e9e10bb748073b05922cb322
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2024-11-24 21:09:23 +0100

    Responsible for creating the visual design of the site using principles of dynamic and responsive layout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Create The Application
+| Create The Applica
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
