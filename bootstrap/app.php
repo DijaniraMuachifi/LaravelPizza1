f00201c26fd88dd4a2295b9e722701f4ade95e8f
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2024-11-24 21:09:50 +0100

    Implementation of registration, login, and logout features, as well as the three-level user system with roles: "guest," "logged-in user," and "admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Create The Applica
+| Create The Applic
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
