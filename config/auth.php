ad7419ca4597a11f34d95cb260e8ac3b666389a6
Amaro-Y2 <amarocartoze@gmail.com>
2024-11-24 21:18:54 +0100

    Creating a PDF document of at least 15 pages explaining how the application works, including screenshots

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authen
+    | Authe
     |--------------------------------------------------------------------------
     |
     | This option controls the default authentication "guard" and password
